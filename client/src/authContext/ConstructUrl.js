5335f42843026e850d40887a37931789a459fb45
Artkabis <artkabis@hotmail.fr>
2021-09-22 18:15:57 +0200

    Update ConstructUrl.js

--- client/src/authContext/ConstructUrl.js
+++ client/src/authContext/ConstructUrl.js
@@ -1,6 +1,6 @@
 const prefix =
   process.env.NODE_ENV === 'production'
-    ? 'http://https://custom-netflix.herokuapp.com/api'
+    ? 'https://custom-netflix.herokuapp.com/api'
     : 'http://localhost:5000/api';
 const ConstructUrl = (url) => {
   return prefix + '/' + url;
