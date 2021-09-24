814d308dae3b3ad9ced1e0a5ecea86e6d6ac80e9
Artkabis <artkabis@hotmail.fr>
2021-09-24 16:51:50 +0200

    change Procfile

--- client/src/authContext/ConstructUrl.js
+++ client/src/authContext/ConstructUrl.js
@@ -1,8 +1,8 @@
 const prefix =
   process.env.NODE_ENV === 'production'
-    ? 'https://custom-netflix.herokuapp.com/api'
-    : 'http://localhost:5000/api';
+    ? 'https://custom-netflix.herokuapp.com/api/'
+    : 'http://localhost:5000/api/';
 const ConstructUrl = (url) => {
-  return prefix + '/' + url;
+  return prefix + url;
 };
 export default ConstructUrl;
