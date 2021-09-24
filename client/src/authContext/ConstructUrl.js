77d97f54eaf2757a1b810342a300da70ca8941e5
Artkabis <artkabis@hotmail.fr>
2021-09-24 17:10:05 +0200

    change proxy local to heroku server

--- client/src/authContext/ConstructUrl.js
+++ client/src/authContext/ConstructUrl.js
@@ -2,7 +2,8 @@ const prefix =
   process.env.NODE_ENV === 'production'
     ? 'https://custom-netflix.herokuapp.com/api/'
     : 'http://localhost:5000/api/';
+console.log('url de construction : ', prefix);
 const ConstructUrl = (url) => {
-  return prefix + url;
+  return 'https://custom-netflix.herokuapp.com/api/' + url;
 };
 export default ConstructUrl;
